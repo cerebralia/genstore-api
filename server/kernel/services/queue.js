fdce9ba7629719acb9fd11ad5ee6084058023d8a
cuongrio07 <cuongrio07@gmail.com>
2019-12-12 14:17:16 +0700

    remove redis

--- server/kernel/services/queue.js
+++ server/kernel/services/queue.js
@@ -5,10 +5,7 @@ exports.create = queueName => (new Queue(queueName, {
   prefix: process.env.QUEUE_PREFIX || 'queue',
   stallInterval: 5000,
   nearTermWindow: 1200000,
-  delayedDebounce: 1000,
-  redis: {
-    url: process.env.REDIS_URL || 'redis://redistogo:40a52946f72e2dd2ead1a02e30ac7e6c@porgy.redistogo.com:11749'
-  },
+  delayedDebounce: 1000, 
   isWorker: true,
   getEvents: true,
   sendEvents: true,
