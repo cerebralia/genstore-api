2e6803886264e7efd7154b5850a9f3a1ec6ac807
cuongrio07 <cuongrio07@gmail.com>
2019-12-12 14:27:34 +0700

    remove job

--- server/kernel/services/queue.js
+++ server/kernel/services/queue.js
@@ -1,17 +1,17 @@
-const Queue = require('bee-queue');
+// const Queue = require('bee-queue');
 
-// shttps:// github.com/bee-queue/bee-queue#settings
-exports.create = queueName => (new Queue(queueName, {
-  prefix: process.env.QUEUE_PREFIX || 'queue',
-  stallInterval: 5000,
-  nearTermWindow: 1200000,
-  delayedDebounce: 1000, 
-  isWorker: true,
-  getEvents: true,
-  sendEvents: true,
-  storeJobs: true,
-  ensureScripts: true,
-  activateDelayedJobs: false,
-  removeOnSuccess: true,
-  removeOnFailure: true
-}));
+// // // shttps:// github.com/bee-queue/bee-queue#settings
+// exports.create = queueName => (new Queue(queueName, {
+//   prefix: process.env.QUEUE_PREFIX || 'queue',
+//   stallInterval: 5000,
+//   nearTermWindow: 1200000,
+//   delayedDebounce: 1000, 
+//   isWorker: true,
+//   getEvents: true,
+//   sendEvents: true,
+//   storeJobs: true,
+//   ensureScripts: true,
+//   activateDelayedJobs: false,
+//   removeOnSuccess: true,
+//   removeOnFailure: true
+// }));
