30a0e8109256c16ee953060bf7d958e876603978
cuongrio07 <cuongrio07@gmail.com>
2019-12-12 11:50:06 +0700

    first commit

---
+++ server/kernel/core/agenda.js
@@ -0,0 +1,26 @@
+const Agenda = require('agenda');
+
+const agenda = new Agenda({
+  name: process.env.AGENDA_COLLECTION_NAME,
+  maxConcurrency: process.env.AGENDA_MAX_CONCURRENCY,
+  db: {
+    address: process.env.MONGO_URI
+  }
+});
+
+// handle events emitted
+// agenda.on('start', job => console.info(`job "${job.attrs.name}" started`));
+agenda.on('complete', () => {
+  console.info(`job "${job.attrs.name}" completed`);
+});
+// agenda.on('success', job => console.info(`job "${job.attrs.name}" succeeded`));
+agenda.on('fail', (err, job) => {
+  // TODO - should move to log
+  console.error(`job "${job.attrs.name}" failed: ${err.message}`, {
+    extra: {
+      job
+    }
+  });
+});
+
+module.exports = agenda;
---
+++ server/module/i18n/services/country.js
@@ -0,0 +1,7 @@
+exports.getCountryByIp = async (ip) => {
+  try {
+    return Promise.resolve('JP');
+  } catch (e) {
+    throw e;
+  }
+};
